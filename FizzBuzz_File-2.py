f3a6dbe171495ea6e3aa4f35a68ff9fec0fff6e8
zz3372 <zz3372@columbia.edu>
2026-02-25 23:38:17 -0500

    Update FizzBuzz_File-2.py

--- FizzBuzz_File-2.py
+++ FizzBuzz_File-2.py
@@ -1,5 +1,14 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    n = np.arange(start, finish + 1)
+    out = n.astype(object)
+
+    m3 = (n % 3 == 0)
+    m5 = (n % 5 == 0)
+
+    out[m3] = "fizz"
+    out[m5] = "buzz"
+    out[m3 & m5] = "fizzbuzz"
+
+    return out.tolist()
