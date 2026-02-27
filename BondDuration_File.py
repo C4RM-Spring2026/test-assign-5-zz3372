d857c011865c48de647ea0d941bdc811efed16d3
zz3372 <zz3372@columbia.edu>
2026-02-27 00:36:55 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -3,7 +3,6 @@ import numpy as np
 def getBondDuration(y, face, couponRate, m, ppy=1):
     n = int(m * ppy)
     k = np.arange(1, n + 1)
-
     c = face * couponRate / ppy
     cf = np.full(n, c, dtype=float)
     cf[-1] += face
