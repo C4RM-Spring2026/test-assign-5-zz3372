308c55291b07943f6fd1e13ab4e767ec83b9e06c
zz3372 <zz3372@columbia.edu>
2026-02-27 00:03:20 -0500

    Delete BondPrice_File-2.py

--- BondPrice_File-2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    n = int(m * ppy)
-    r = y / ppy
-    t = np.arange(1, n + 1, dtype=float)
-    df = (1.0 + r) ** (-t)
-    c = face * couponRate / ppy
-    return c * df.sum() + face * df[-1]
